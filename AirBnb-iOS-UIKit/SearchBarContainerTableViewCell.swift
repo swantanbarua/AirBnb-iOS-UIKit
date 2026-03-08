1bd60877e18a0872e7663a363bb8041219b2ca43
Swantan Barua <work.swantan@gmail.com>
2026-03-08 13:28:49 +0530

    Add SearchBarContainerTableViewCell with default UITableViewCell methods

---
+++ AirBnb-iOS-UIKit/SearchBarContainerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchBarContainerTableViewCell.swift
+//  AirBnb-iOS-UIKit
+//
+//  Created by Swantan Barua on 08/03/26.
+//
+
+import UIKit
+
+class SearchBarContainerTableViewCell: UITableViewCell {
+
+    // MARK: - LIFECYCLE METHODS
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(
+        _ selected: Bool,
+        animated: Bool
+    ) {
+        super.setSelected(selected, animated: animated)
+    }
+}
